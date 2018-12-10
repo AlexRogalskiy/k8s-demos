8a4a907c4ac7594a93f02044a78b18dea1ce24d1
Ivan Portyankin <ivan.portyankin@gmail.com>
2018-12-10 17:52:29 -0500

    ktor rest app.:

--- ktor-rest/src/main/kotlin/notes/NotesApp.kt
+++ ktor-rest/src/main/kotlin/notes/NotesApp.kt
@@ -1,18 +1,34 @@
 package notes
 
+import com.fasterxml.jackson.databind.SerializationFeature
 import io.ktor.application.*
-import io.ktor.http.*
+import io.ktor.features.ContentNegotiation
+import io.ktor.jackson.jackson
+import io.ktor.request.receiveText
 import io.ktor.response.*
 import io.ktor.routing.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 
-fun main(args: Array<String>) {
-    embeddedServer(Netty, 8080) {
-        routing {
-            get("/") {
-                call.respondText("Notes RESTful service", ContentType.Text.Html)
-            }
+val notes = mutableListOf<String>()
+
+fun Application.module() {
+    install(ContentNegotiation) {
+        jackson {
+            enable(SerializationFeature.INDENT_OUTPUT) // Pretty Prints the JSON
         }
-    }.start(wait = true)
+    }
+    routing {
+        get("/notes") {
+            call.respond(mapOf("notes" to notes))
+        }
+
+        post("/notes") {
+            notes.add(call.receiveText())
+        }
+    }
+}
+
+fun main(args: Array<String>) {
+    embeddedServer(Netty, 8080, watchPaths = listOf("NotesAppKt"), module = Application::module).start()
 }
