552d39ff841808eb182155a7041dabbe1151d9aa
Ivan Portyankin <ivan.portyankin@gmail.com>
2018-12-10 11:56:46 -0500

    adding simple java print.

---
+++ k8s-simple-java-print/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("Hello World!");
+
+        while (true) {
+            System.out.println("Hello from Java main()!");
+            System.out.println("this is a change! random number -> " + Math.round(Math.random()*20));
+            Thread.sleep(1000);
+        }
+    }
+
+}
+
+class NoChangesClass {
+    private String value;
+}
